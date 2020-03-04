2a928306f06855360213a6f3ac74533759866eb5
Jorge Padilla <jorgep4dill4@gmail.com>
2020-03-03 22:59:38 -0600

    volumetric weight fix

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,7 +1,7 @@
 class Shipment < ApplicationRecord
     validates :length, :width, :height, presence: true
     def volumetric_weight
-        if self.length && self.width
+        if self.length && self.width && self.height
           self.volumetric_weight = (length * width* height)/166
         else
             self.volumetric_weight = "0.0".to_d
