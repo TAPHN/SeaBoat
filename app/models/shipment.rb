d1067ab357d4d06df2c661106ebba6b89e9498ce
Jorge Padilla <jorgep4dill4@gmail.com>
2020-03-04 07:37:18 -0600

    show only 2 decimal places for weight

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,10 +1,10 @@
 class Shipment < ApplicationRecord
-    validates :length, :width, :height, presence: true
+    validates :length, :width, :height, :weight, presence: true
     def volumetric_weight
         if self.length && self.width && self.height
           self.volumetric_weight = (length * width* height)/166
         else
-            self.volumetric_weight = "0.0".to_d
+            self.volumetric_weight = "0.00".to_d
         end
     end
 end
